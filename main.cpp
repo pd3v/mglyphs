797813acd39c65de55b65277b189848596e649dc
pd3v <paulo.develop@gmail.com>
2022-02-03 12:06:53 +0000

    add code testing diatonic lib usage

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "externals/diatonic/include/diatonic.h"
+
+using namespace scale_;
 
 int main() {
-  std::cout << "c++ and actions." << std::endl;
+  std::cout << "c++ and actions + submodule diatonic" << std::endl;
+
+  // create scales
+  auto DSharpMajor = scale(Ds,scale_::major);
+  auto DSharpPentaMajorOctave4 = scale("ds4",scale_::pentatonicmajor);
+
+  std::cout << "D# Major 2nd note is:" << DSharpMajor.at(1) << std::endl;
+
   return 0;
 }
