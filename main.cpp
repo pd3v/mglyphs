2433b66fcdf0d4ce0bf6ed350256d7e7e7f27771
pd3v <paulo.develop@gmail.com>
2024-06-01 15:31:05 +0100

    scales' MIDI printing

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 
 using namespace scale_;
 
+void printMIDI(const scaleT& scale) {
+	for (auto& note : scale)
+		std::cout << note << ' ';
+
+	std::cout << '\n';
+}
+
 int main() {
-  std::cout << "c++ and actions + submodule diatonic" << std::endl;
 
   // create D# Major scale
   //auto DSharpMajor = scale(Ds,scale_::major);
@@ -12,10 +18,27 @@ int main() {
   //std::cout << "D# Major major 3rd note is:" << DSharpMajor.at(4) << std::endl;
 
 	// create scales
-	auto DSharpMajor = scale(Ds,scale_::major);
- 	auto DSharpPentaMajorOctave4 = scale("a1",scale_::pentatonicminor);
-	for (auto& note : DSharpPentaMajorOctave4)
-		std::cout << note << '\n';
+	// auto CMajorScale = scale(C,scale_::major);
+ 	//auto DSharpPentaMajorOctave4 = scale("a1",scale_::pentatonicminor);
 
+	std::cout << "\nscales MIDI notes\n-----------------\n\n";
+
+	std::cout << "Chromatic scale:\t";
+	printMIDI(scale(C,scale_::chromatic));
+	
+	std::cout << "C Major scale:\t\t";
+	printMIDI(scale(C,scale_::major));
+	
+	std::cout << "C Minor scale:\t\t";
+	printMIDI(scale(C,scale_::minor));
+
+	std::cout << "Whole notes scale:\t";
+	printMIDI(scale(C,scale_::whole));
+
+	std::cout << "C Harmonic Minor scale:\t";
+	printMIDI(scale(C,scale_::harmonicminor));
+	
+	std::cout << '\n';
+	
   return 0;
 }
